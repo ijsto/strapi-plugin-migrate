9604c0cc62a4f3aa107bea5ef7ef5b02d3ec80c8
ScottAgirs <scott@ijs.to>
2020-08-04 22:07:34 -0400

    [improve] admin UI
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- admin/src/components/UploadFileForm/index.js
+++ admin/src/components/UploadFileForm/index.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
-import { Button, LoadingBar, Textarea } from '@buffetjs/core'
-import { request } from "strapi-helper-plugin";
-import Row from "../Row";
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { useState } from 'react';
+import { Button, Textarea } from '@buffetjs/core';
+import { request } from 'strapi-helper-plugin';
+import Row from '../Row';
+import CurrentRoles from '../CurrentRoles';
 
 const UploadFileForm = () => {
-  const [postgresString, setPostgresString] = useState("");
-  const [retrievedPostgresString, setRetrievedPostgresString] = useState("");
+  const [postgresString, setPostgresString] = useState('');
+  const [retrievedPostgresString, setRetrievedPostgresString] = useState('');
+  const [currentRoles, setCurrentRoles] = useState();
 
   const [loadingRetrieve, setLoadingRetrieve] = useState(false);
   const [loadingSubmit, setLoadingSubmit] = useState(false);
@@ -18,75 +21,88 @@ const UploadFileForm = () => {
   const [copySuccess, setCopySuccess] = useState(false);
 
   const handleRetrieve = async () => {
-    setErrorRetrieve(false)
-    setLoadingRetrieve(true)
+    setErrorRetrieve(false);
+    setLoadingRetrieve(true);
 
     try {
-      const response = await request("/migrate/retrieveSqlString", { method: "GET" });
+      const response = await request('/migrate/retrieveSqlString', {
+        method: 'POST',
+        body: { updatedRoles: currentRoles, type: 'postgres' },
+      });
 
       if (response) {
-        const cleanedString = response.generatedString.replace(/\\\//g, "/")
+        const cleanedString = response.generatedString.replace(/\\\//g, '/');
 
-        setRetrievedPostgresString(`${cleanedString};`)
+        setRetrievedPostgresString(`${cleanedString};`);
         setLoadingRetrieve(false);
-        setPostgresString("");
+        setPostgresString('');
       }
     } catch (e) {
-      console.log("Error: ", e)
-      setErrorRetrieve(true)
+      console.log('Error: ', e);
+      setErrorRetrieve(true);
       setLoadingRetrieve(false);
     }
-  }
+  };
 
   const handleSubmit = async () => {
     if (postgresString?.length < 10) return;
 
-    setErrorSubmit(false)
-    setLoadingSubmit(true)
+    setErrorSubmit(false);
+    setLoadingSubmit(true);
 
     try {
-      const response = await request("/migrate/uploadPostgres", {
-        method: "POST",
-        body: { postgresString }
+      const response = await request('/migrate/uploadPostgres', {
+        method: 'POST',
+        body: { postgresString },
       });
 
       if (response.success) {
-        setPostgresString("");
+        setPostgresString('');
         setLoadingSubmit(false);
-        setSubmitSuccess(true)
+        setSubmitSuccess(true);
       }
     } catch (e) {
-      console.log("Error: ", e)
-      setErrorSubmit(true)
+      console.log('Error: ', e);
+      setErrorSubmit(true);
       setLoadingSubmit(false);
     }
-  }
+  };
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(retrievedPostgresString)
+    navigator.clipboard.writeText(retrievedPostgresString);
     setCopySuccess(true);
-  }
+  };
 
   return (
+    <div style={{ padding: '1.8rem 1.5rem' }}>
+      <h1 style={{ marginTop: '3rem' }}>Export Permissions</h1>
+      <div>
+        You will get a raw SQL query that you will be able to paste in another
+        environment (for example staging, production).
+      </div>
 
-    <div style={{ padding: "1.8rem 1.5rem" }}>
-      <h1 style={{ marginTop: "3rem" }}>Export Permissions</h1>
-      <div>You will get a raw SQL query that you will be able to paste in another environment (for example staging, production).</div>
+      <Row>
+        <CurrentRoles
+          currentRoles={currentRoles}
+          setCurrentRoles={setCurrentRoles}
+        />
+      </Row>
 
       <Row>
         <Button
           disabled={loadingRetrieve}
           isLoading={loadingRetrieve}
-          label={'Get now'}
+          label="Get now"
           onClick={handleRetrieve}
           style={{ marginRight: 10 }}
         />
-        {retrievedPostgresString &&
+        {retrievedPostgresString && (
           <Button
-            color={copySuccess ? "success" : 'primary'}
-            label={copySuccess ? "Copied!" : 'Copy Permissions query'}
+            color={copySuccess ? 'success' : 'primary'}
+            label={copySuccess ? 'Copied!' : 'Copy Permissions query'}
             onClick={handleCopy}
-          />}
+          />
+        )}
       </Row>
 
       <Row>
@@ -95,12 +111,12 @@ const UploadFileForm = () => {
 
       {!errorRetrieve && retrievedPostgresString && !loadingRetrieve && (
         <Row>
-          <div style={{ color: "#28a745" }}>User permissions retrieved.</div>
+          <div style={{ color: '#28a745' }}>User permissions retrieved.</div>
         </Row>
       )}
       {errorRetrieve && (
         <Row>
-          <div style={{ color: "#dc3545" }}>Uh-oh! Something went wrong.</div>
+          <div style={{ color: '#dc3545' }}>Uh-oh! Something went wrong.</div>
         </Row>
       )}
 
@@ -118,12 +134,12 @@ const UploadFileForm = () => {
 
       {!errorSubmit && submitSuccess && !loadingSubmit && (
         <Row>
-          <div style={{ color: "#28a745" }}>User permissions imported!</div>
+          <div style={{ color: '#28a745' }}>User permissions imported!</div>
         </Row>
       )}
       {errorSubmit && (
         <Row>
-          <div style={{ color: "#dc3545" }}>Uh-oh! Something went wrong.</div>
+          <div style={{ color: '#dc3545' }}>Uh-oh! Something went wrong.</div>
         </Row>
       )}
 
@@ -131,13 +147,12 @@ const UploadFileForm = () => {
         <Button
           isLoading={loadingSubmit}
           disabled={loadingSubmit}
-          label={'Run it'}
+          label="Run it"
           onClick={handleSubmit}
         />
       </Row>
-    </div >
+    </div>
   );
-
-}
+};
 
 export default UploadFileForm;
