62918564833c12d62b0ff32f8ecd0dc9bb607616
ScottAgirs <scott@ijs.to>
2020-08-06 19:41:59 -0400

    [init] preferred-roles [ci skip]
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ controllers/preferred-roles.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  find: async () => {
+    console.log('Hey, Scott here from iJS!');
+  },
+};
