728cbf04fd29672aed11d2f22be6eeb876f99070
ScottAgirs <scott@ijs.to>
2020-08-06 22:35:02 -0400

    [init] HomePage /> layout
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -6,12 +6,28 @@
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-// import pluginId from '../../pluginId';
-import UserPermissions from '../../components/UserPermissions';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ViewContainer } from 'strapi-helper-plugin';
 
-const HomePage = () => {
-  return <UserPermissions />;
+// import PropTypes from 'prop-types';
+import UserPermissions from '../../components/UserPermissions';
+import pluginId from '../../pluginId';
+import Sidebar from '../Sidebar';
+
+const HomePage = props => {
+  return (
+    <ViewContainer className={pluginId}>
+      <div className="container-fluid">
+        <div className="row">
+          <Sidebar />
+
+          <div className="col-md-9">
+            <UserPermissions {...props} />
+          </div>
+        </div>
+      </div>
+    </ViewContainer>
+  );
 };
 
 export default memo(HomePage);
