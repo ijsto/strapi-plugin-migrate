6eccb4305fcb4e65367500a975cb4b445dd788d8
ScottAgirs <scott@ijs.to>
2021-02-18 15:59:53 -0500

    feat(Pages): Init UserPermissionsPage
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ admin/src/containers/UserPermissionsPage/index.js
@@ -0,0 +1,45 @@
+/*
+ *
+ * UserPermissionsPage
+ *
+ */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import React, { memo } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Padded } from '@buffetjs/core';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ViewContainer, useGlobalContext } from 'strapi-helper-plugin';
+
+// import PropTypes from 'prop-types';
+import UserPermissions from '../../components/UserPermissions';
+import pluginId from '../../pluginId';
+import Sidebar from '../Sidebar';
+import getTrad from '../../utils/getTrad';
+
+const UserPermissionsPage = () => {
+  const { formatMessage } = useGlobalContext();
+
+  return (
+    <ViewContainer className={pluginId}>
+      <div className="container-fluid">
+        <div className="row">
+          <Sidebar />
+
+          <div className="col-md-9">
+            <Padded top size="smd">
+              <h1>
+                {formatMessage({
+                  id: getTrad(`UserPermissions.header.title`),
+                })}
+              </h1>
+              <UserPermissions />
+            </Padded>
+          </div>
+        </div>
+      </div>
+    </ViewContainer>
+  );
+};
+
+export default memo(UserPermissionsPage);
