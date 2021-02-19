c1fb4b098e1590671fac83432e5f2227559d7c7c
ScottAgirs <scott@ijs.to>
2021-02-19 01:42:40 -0500

    chore: Cleanup unused in ExportPerms />
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- admin/src/components/UserPermissions/ExportPermissions.js
+++ admin/src/components/UserPermissions/ExportPermissions.js
@@ -1,55 +1,14 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import styled from 'styled-components';
-/* eslint-disable import/no-extraneous-dependencies */
-import React, { useState } from 'react';
-import { useGlobalContext, request } from 'strapi-helper-plugin';
+import React from 'react';
+import { useGlobalContext } from 'strapi-helper-plugin';
 
 import Flex from '../layout/Flex';
-import Row from '../layout/Row';
-import FadedCard from '../data-display/FadedCard';
 import getTrad from '../../utils/getTrad';
 import ExportPermissionsText from './ExportPermissionsText';
 import ExportPermissionsFile from './ExportPermissionsFile';
 
-const StyledFadedCardFile = styled(FadedCard)`
-  &:before {
-    content: '\f56d';
-    color: #adadad;
-  }
-`;
 
 const ExportPermissions = () => {
   const { formatMessage } = useGlobalContext();
-  const [retrievedPostgresString, setRetrievedPostgresString] = useState('');
-
-  const [loadingRetrieve, setLoadingRetrieve] = useState(false);
-  const [errorRetrieve, setErrorRetrieve] = useState(null);
-
-  const [copySuccess, setCopySuccess] = useState(false);
-
-  const handleRetrieve = async () => {
-    setErrorRetrieve(false);
-    setLoadingRetrieve(true);
-
-    try {
-      const response = await request('/migrate/retrieveSqlString', {
-        method: 'POST',
-        body: { type: 'postgres' },
-      });
-
-      if (response) {
-        const cleanedString = response.generatedString.replace(/\\\//g, '/');
-
-        setRetrievedPostgresString(`${cleanedString};`);
-        setLoadingRetrieve(false);
-      }
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.log('Error: ', e);
-      setErrorRetrieve(true);
-      setLoadingRetrieve(false);
-    }
-  };
 
   return (
     <div>
