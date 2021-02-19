e30a043ec5fa199eae191f4de89195ec6f7ad455
ScottAgirs <scott@ijs.to>
2021-02-18 20:58:44 -0500

    feat(HomePage): Implement PageContainer />
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -7,37 +7,24 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React, { memo } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Padded } from '@buffetjs/core';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { ViewContainer, useGlobalContext } from 'strapi-helper-plugin';
+import { useGlobalContext } from 'strapi-helper-plugin';
 
 // import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
-import Sidebar from '../Sidebar';
 import getTrad from '../../utils/getTrad';
+import PageContainer from '../../components/layout/PageContainer';
+
 
 const HomePage = () => {
   const { formatMessage } = useGlobalContext();
+  const headerProps = {
+    content: formatMessage({ id: getTrad(`HomePage.header.description`) }),
+    title: { label: formatMessage({ id: getTrad(`HomePage.header.title`) }) },
+  };
 
   return (
-    <ViewContainer className={pluginId}>
-      <div className="container-fluid">
-        <div className="row">
-          <Sidebar />
-
-          <div className="col-md-9">
-            <Padded top left right size="smd">
-              <h1>
-                {formatMessage({
-                  id: getTrad(`HomePage.header.title`),
-                })}
-              </h1>
-              Strapi Migrate Dashboard
-            </Padded>
-          </div>
-        </div>
-      </div>
-    </ViewContainer>
+    <PageContainer headerProps={headerProps}>
+      <div>Strapi Migrate Dashboard</div>
+    </PageContainer>
   );
 };
 
