c460b426e4f303c402f42f9951cf33ec95e55ad8
ScottAgirs <scott@ijs.to>
2021-02-21 00:22:00 -0500

    chore: [nts] linting
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- admin/src/components/Dashboard/index.js
+++ admin/src/components/Dashboard/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-import Grid from "../layout/Grid"
+import Grid from '../layout/Grid';
 
 import CollectionTypeStats from './CollectionTypeStats';
 import UserPermissionStats from './UserPermissionStats';
 import Box from '../layout/Box';
 
 const Dashboard = () => {
-  return ( <>
-  <h1>Dashboard</h1>
-  
-  <Box py="10px">
-    <Grid>
-      <UserPermissionStats />
+  return (
+    <>
+      <h1>Dashboard</h1>
 
-      <CollectionTypeStats />
-    </Grid>
-  </Box>
+      <Box py="10px">
+        <Grid>
+          <UserPermissionStats />
 
-  </> );
-}
- 
+          <CollectionTypeStats />
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
 export default Dashboard;
