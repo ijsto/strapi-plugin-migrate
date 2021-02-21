fd20c3d572d8c4a353c0ddb549be5b763474c70d
ScottAgirs <scott@ijs.to>
2021-02-20 21:32:11 -0500

    feat(Components): Init Box />
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ admin/src/components/layout/Box.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledBox = styled.div`
+  padding: ${({ p, px, py, padding }) =>
+    px || py ? `${py || 0} ${px || 0}` : p || padding};
+  margin: ${({ m, mx, my, margin }) =>
+    mx || my ? `${my || 0} ${mx || 0}` : m || margin};
+`;
+
+const Box = ({ children, ...rest }) => {
+  return <StyledBox {...rest}>{children}</StyledBox>;
+};
+
+export default Box;
