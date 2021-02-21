158377b83cece111b491d99503edb1fe6bee96fa
ScottAgirs <scott@ijs.to>
2021-02-21 15:23:31 -0500

    refactor(Permissions): Implement new layout
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- admin/src/components/UserPermissions/ImportPermissions.js
+++ admin/src/components/UserPermissions/ImportPermissions.js
@@ -1,80 +1,20 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import React, { useState } from 'react';
-import { Button, Textarea } from '@buffetjs/core';
+import React from 'react';
+import { Flex } from '@buffetjs/core';
+import { useGlobalContext } from 'strapi-helper-plugin';
 
-import { useGlobalContext, request } from 'strapi-helper-plugin';
-
-import Row from '../layout/Row';
-import getTrad from '../../utils/getTrad';
+import ImportPermissionsText from './ImportPermissionsText';
+import ImportPermissionsFile from './ImportPermissionsFile';
 
 const ImportPermissions = () => {
   const { formatMessage } = useGlobalContext();
-  const [postgresString, setPostgresString] = useState('');
-
-  const [loadingSubmit, setLoadingSubmit] = useState(false);
-  const [errorSubmit, setErrorSubmit] = useState(null);
-  const [submitSuccess, setSubmitSuccess] = useState(false);
-
-  const handleSubmit = async () => {
-    if (postgresString?.length < 10) return;
-
-    setErrorSubmit(false);
-    setLoadingSubmit(true);
-
-    try {
-      const response = await request('/migrate/uploadPostgres', {
-        method: 'POST',
-        body: { postgresString },
-      });
-
-      if (response.success) {
-        setPostgresString('');
-        setLoadingSubmit(false);
-        setSubmitSuccess(true);
-      }
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.log('Error: ', e);
-      setErrorSubmit(true);
-      setLoadingSubmit(false);
-    }
-  };
 
   return (
     <div>
-      <h1>
-        {formatMessage({ id: getTrad(`UserPermissions.import.title`) })}
-      </h1>
+      <Flex>
+        <ImportPermissionsText />
 
-      <div>
-        {formatMessage({ id: getTrad(`UserPermissions.import.description`) })}
-        </div>
-      <Row>
-        <Textarea
-          name="import-sql-string"
-          disabled={loadingSubmit}
-          onChange={({ target: { value } }) => setPostgresString(value)}
-          value={postgresString}
-        />
-      </Row>
-      {!errorSubmit && submitSuccess && !loadingSubmit && (
-        <Row>
-          <div style={{ color: '#28a745' }}>User permissions imported!</div>
-        </Row>
-      )}
-      {errorSubmit && (
-        <Row>
-          <div style={{ color: '#dc3545' }}>Uh-oh! Something went wrong.</div>
-        </Row>
-      )}
-      <Row>
-        <Button
-          isLoading={loadingSubmit}
-          disabled={loadingSubmit}
-          label="Run it"
-          onClick={handleSubmit}
-        />
-      </Row>
+        <ImportPermissionsFile />
+      </Flex>
     </div>
   );
 };
