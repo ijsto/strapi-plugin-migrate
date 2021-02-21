ea59920dc813f37884a2367fca7b060a2915f8cd
ScottAgirs <scott@ijs.to>
2021-02-20 23:23:23 -0500

    feat(Utils): Init downloadNamedJson
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ admin/src/utils/downloadNamedJson.js
@@ -0,0 +1,17 @@
+export default function downloadNamedJson(exportFile, exportFileName) {
+  const json = exportFile;
+
+  const data = `text/json;charset=utf-8,${encodeURIComponent(
+    JSON.stringify(json)
+  )}`;
+  const a = document.createElement('a');
+  a.href = `data:${data}`;
+  a.download = `${exportFileName}.json`;
+  a.innerHTML = 'download JSON';
+
+  const container = document.getElementById('download');
+  container.appendChild(a);
+  a.click();
+
+  a.remove();
+}
