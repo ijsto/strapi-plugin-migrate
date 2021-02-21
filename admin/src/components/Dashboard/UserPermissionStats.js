b075dfbf310ff2355145cdd9b0f42ca4fbbb6a73
ScottAgirs <scott@ijs.to>
2021-02-21 17:35:32 -0500

    feat(Permissions): Hook up basic UserPermissionStats func
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- admin/src/components/Dashboard/UserPermissionStats.js
+++ admin/src/components/Dashboard/UserPermissionStats.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@buffetjs/core';
 import { Link } from 'react-router-dom';
+import { request } from 'strapi-helper-plugin';
 
 import Box from '../layout/Box';
 import Card from '../data-display/Card';
@@ -9,15 +10,44 @@ import CardInfoList from '../data-display/CardInfoList';
 import basePluginUrl from '../../basePluginUrl';
 
 const UserPermissionStats = () => {
-  const infoList = [{ label: 'Total Roles', value: '??' }];
+  const [allRoles, setAllRoles] = useState([]);
+  const handlePermissionsExport = async () => {
+    try {
+      const userRoles = await request(`/migrate/getPermissions`);
+
+      if (userRoles) setAllRoles(userRoles);
+    } catch (err) {
+      strapi.notification.toggle({
+        message: err.toString(),
+        timeout: 3500,
+        title: 'Yikes!',
+        type: 'warning',
+      });
+    }
+  };
+
+  useEffect(() => {
+    handlePermissionsExport();
+  }, []);
+
+  const infoList = [{ label: 'Total Roles', value: allRoles?.length || 0 }];
 
   return (
     <Card padding="0 20px">
       <h3 style={{ marginTop: 16 }}>User Permissions</h3>
 
-      <Box my="20px">
+      <Box>
         <CardInfoList list={infoList} />
+      </Box>
 
+      <Box margin="0 0 20px 0">
+        You can get up to 30 or even unlimited amount of roles with Strapi paid
+        plans. See{' '}
+        <a href="https://strapi.io/pricing">https://strapi.io/pricing</a> for
+        more info.
+      </Box>
+
+      <Box my="20px">
         <Link to={`${basePluginUrl}/user-permissions`}>
           <Button>Migrate Permissions</Button>
         </Link>
