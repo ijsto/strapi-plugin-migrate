3a6aa28c4117ea7b6179a2f318b3fef274a568dd
ScottAgirs <scott@ijs.to>
2021-02-21 17:08:30 -0500

    feat(Content): [WIP] Init markup
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ controllers/spm-file-export-content.js
@@ -0,0 +1,57 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+/**
+ * migrate.js controller
+ *
+ * @description: TODO:
+ */
+
+module.exports = {
+  /**
+   * Default action.
+   * @description: TODO:
+   *
+   * @return {Object}
+   */
+
+  index: async ctx => {
+    // Send 200 `ok`
+
+    ctx.send({
+      message: 'ok',
+    });
+  },
+  downloadContent: async ctx => {
+    // @NOTE: This controller is work in progress.
+    // To track progress/contribute/submit a PR
+    // visit this github link: https://github.com/ijsto/strapi-plugin-migrate/issues/6
+    const { user } = ctx.state;
+    if (!user.roles || user.roles[0].id !== 1) {
+      return ctx.unauthorized('You must be an admin to export content.');
+    }
+
+    const models = Object.values(strapi.models);
+
+    const modelsData = await Promise.all(
+      models.map(async model => {
+        if (!model.modelName) return;
+
+        const modelAPI = strapi.query(model.modelName);
+        const allModelEntries = await modelAPI.find({ _limit: -1 });
+
+        if (Array.isArray(allModelEntries)) {
+          allModelEntries.collectionName = model.modelName;
+          return { [model.modelName]: allModelEntries };
+        }
+
+        const sanitized = allModelEntries
+          .map(m => sanitizeEntity(m, { model }))
+          .filter(collection => collection);
+
+        return sanitized;
+      })
+    );
+
+    return modelsData;
+  },
+};
---
+++ controllers/spm-file-import-content.js
@@ -0,0 +1,65 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+/**
+ * spm-file-import-content.js controller
+ *
+ * @description: TODO:
+ */
+
+module.exports = {
+  /**
+   * Default action.
+   * @description: TODO:
+   *
+   * @return {Object}
+   */
+
+  index: async ctx => {
+    // Send 200 `ok`
+
+    ctx.send({
+      message: 'ok',
+    });
+  },
+  uploadContent: async ctx => {
+    // @NOTE: This controller is work in progress.
+    // To track progress/contribute/submit a PR
+    // visit this github link: https://github.com/ijsto/strapi-plugin-migrate/issues/6
+
+    const { collectionTypes } = ctx.request.body;
+    const { user } = ctx.state;
+
+    if (user.roles[0].id != 1) {
+      return ctx.unauthorized('You must be an admin to import content.');
+    }
+
+    const collectionTypesKeys = Object.keys(collectionTypes);
+
+    await Promise.all(
+      collectionTypesKeys.map(async typeKey => {
+        const model = strapi.models[typeKey];
+        const typeService = strapi.services[typeKey];
+
+        const typeData = collectionTypes[typeKey];
+        typeData.map(async entryData => {
+          const sanitizedData = sanitizeEntity(entryData, { model });
+          delete sanitizedData.id;
+          delete sanitizedData.updated_by;
+          delete sanitizedData.created_by;
+
+          if (model.kind === 'collectionType') {
+            const found = await typeService.find(sanitizedData);
+
+            if (!found.length) {
+              await typeService.create(sanitizedData);
+            } else {
+              await typeService.update(entryData.id, sanitizedData);
+            }
+          }
+        });
+      })
+    );
+
+    return { success: true };
+  },
+};
