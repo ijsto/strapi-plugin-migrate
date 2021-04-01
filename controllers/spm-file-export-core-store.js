3619a23899b364b2b58ca7dc05a13306ccd8d1ce
Mat Gadd <mat.gadd@universlabs.co.uk>
2021-04-01 17:26:39 +0100

    Update core store export to allow an optional model to be specified

--- controllers/spm-file-export-core-store.js
+++ controllers/spm-file-export-core-store.js
@@ -23,12 +23,20 @@ module.exports = {
   },
   getCoreStoreJSON: async ctx => {
     const { user } = ctx.state;
+    const { model } = ctx.params;
+
     if (user.roles[0].code !== 'strapi-super-admin') {
-      return ctx.unauthorized('You must be an admin to export permissions.');
+      return ctx.unauthorized('You must be an admin to export settings and layouts.');
+    }
+
+    const queryOptions = { _limit: -1 };
+    if (model) {
+      const prefix = 'plugin_content_manager_configuration_content_types::';
+      queryOptions.key = prefix + model;
     }
 
     const coreStoreAPI = strapi.query('core_store');
-    const coreStore = await coreStoreAPI.find({ _limit: -1 });
+    const coreStore = await coreStoreAPI.find(queryOptions);
     const withoutIds = coreStore.map(({id, ...coreStoreNoIds}) => coreStoreNoIds)
 
     return withoutIds;
