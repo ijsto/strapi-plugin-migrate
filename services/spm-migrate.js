f855eca0fb033b1ef52a1573fac5fb846600e083
ScottAgirs <scott@ijs.to>
2021-04-13 18:53:51 -0400

    chore: remove deprecated
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- services/spm-migrate.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * strapi-plugin-migrate.js service
- *
- * @description: A set of functions similar to controller's actions to avoid code duplication.
- */
-
-module.exports = {};
