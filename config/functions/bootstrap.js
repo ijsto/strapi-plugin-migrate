19faee41bdae170b3eee9677ab20aa1360d8b47d
p_0g_8mm3_ <37022952+pr0gr8mm3r@users.noreply.github.com>
2021-05-17 20:47:20 +0200

    hardcode plugin name (wip)

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,19 +1,15 @@
-const pluginPkg = require('../../package.json');
-
 module.exports = async () => {
     registerPermissionActions();
 };
 
 const registerPermissionActions = () => {
-    const { name } = pluginPkg;
-
     const actions = [
         {
             section: 'settings',
             displayName: 'Access the Migrate pages in the settings',
             uid: 'settings.access',
             category: 'migrate',
-            pluginName: name
+            pluginName: 'migrate'
         },
     ];
 
