29df73e9410a52daad5cba775863241f0ab528ea
ScottAgirs <scott@ijs.to>
2021-06-26 16:50:20 -0400

    feat(Errors): Add permission type validator
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- controllers/spm-file-import-permissions.js
+++ controllers/spm-file-import-permissions.js
@@ -38,32 +38,102 @@ module.exports = {
     if (user.roles[0].code !== 'strapi-super-admin') {
       return ctx.unauthorized('You must be an admin to import permissions.');
     }
-    
-    const { rolesAndPermissions } = ctx.request.body;
-    
+
+    const { rolesAndPermissions: rolesFromSource } = ctx.request.body;
+    if (!Array.isArray(rolesFromSource))
+      throw new Error('Your file format is invalid. Must be an array.');
+
     const service =
       strapi.plugins['users-permissions'].services.userspermissions;
 
     const isValidJSON = await Promise.all(
-      rolesAndPermissions.map(async role => roleYupSchema.isValid(role))
+      rolesFromSource.map(async role => roleYupSchema.isValid(role))
     ).then(values => values.every(Boolean));
 
     if (!isValidJSON) {
       return ctx.throw(400, 'Please provide a valid JSON');
     }
-    
+
+    const [currentEnvRoles, plugins] = await Promise.all([
+      service.getRoles(),
+      service.getPlugins(),
+    ]);
+
+    // Backfill/conform roles
     await Promise.all(
-      rolesAndPermissions.map(async data => {
-        const role = await strapi
-          .query('role', 'users-permissions')
-          .findOne({ name: data.name });
+      rolesFromSource.map(async roleFromSource => {
+        const roleToUpdate = currentEnvRoles.find(
+          role => role.name === roleFromSource.name
+        );
 
-        const users = role ? role.users : [];
-        data.users = users;
+        const users = roleToUpdate ? roleToUpdate.users : [];
+        roleFromSource.users = users;
 
-        if (!role) return service.createRole(data);
+        // Backfill missing role.
+        if (!roleToUpdate)
+          return service.createRole(roleFromSource).catch(err => {
+            console.error(
+              '[Error:Migrate] Failed to backfill missing role .createRole(), ',
+              err
+            );
+          });
+      })
+    );
 
-        return service.updateRole(role.id, data);
+    // Verify that Source and Current Environment contains the same permissions.
+    const sampleSrcRole = rolesFromSource[0];
+    const sampleCurrRole = currentEnvRoles.find(role => {
+      return role.name === sampleSrcRole.name;
+    });
+    const sampleCurrentEnvRole = await service.getRole(
+      sampleCurrRole.id,
+      plugins
+    );
+
+    const sampleSrcPermTypes = Object.keys(sampleSrcRole.permissions);
+    const sampleCurrPermTypes = Object.keys(sampleCurrentEnvRole.permissions);
+
+    if (sampleSrcPermTypes.length !== sampleCurrPermTypes.length) {
+      // Get the array that's bigger
+      // Check which value doesn't exist in smaller array
+
+      const extraPermsArray =
+        sampleSrcPermTypes.length > sampleCurrPermTypes.length
+          ? sampleSrcPermTypes
+          : sampleCurrPermTypes;
+
+      const missingPermsArray =
+        sampleSrcPermTypes.length < sampleCurrPermTypes.length
+          ? sampleSrcPermTypes
+          : sampleCurrPermTypes;
+
+      extraPermsArray.map(permission => {
+        if (!missingPermsArray.includes(permission)) {
+          // "This permission is missing in one of the envs."
+          throw new Error(
+            `The permission "${permission}" is missing from one of the Environments.`
+          );
+        }
+
+        return null;
+      });
+    }
+
+    // Updates roles
+    await Promise.all(
+      rolesFromSource.map(async roleFromSource => {
+        const roleToUpdate = currentEnvRoles.find(
+          role => role.name === roleFromSource.name
+        );
+
+        const users = roleToUpdate ? roleToUpdate.users : [];
+        roleFromSource.users = users;
+
+        return service
+          .updateRole(roleToUpdate.id, roleFromSource)
+          .catch(err => {
+            console.error('[Error:Migrate] Failed to .updateRole(), ', err);
+          });
       })
     );
 
